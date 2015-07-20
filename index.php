3d3f4433fbd92c8fe686f0e9c946067af7389639
tickettoeverything <tickettoeverything@gmail.com>
2015-07-20 14:52:55 -0400

    update index.php and image file

--- index.php
+++ index.php
@@ -2,224 +2,124 @@
 <html>
 <head>
 <title>Page Title</title>
+<link rel="stylesheet" type="text/css" href="indexCss.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
+<script>
 
-<style>
+	$(document).ready(function(){
+      // Run our swapImages() function every 5secs
+      setInterval('swapImages()', 5000);
+    });
+    function swapImages(){
+      var $active = $('#index-gal .active');
+      var $next = ($('#index-gal .active').next().length > 0) ? $('#index-gal .active').next() : $('#index-gal img:first');
+      $active.fadeOut(function(){
+      $active.removeClass('active');
+      $next.fadeIn().addClass('active');
+      });
+    }
+  
+ </script>
 
-body {
-    background-color: #b0c4de;
-	height: 101%;
-    margin: 0px;
-	width: 100%;
-	overflow: auto;
-	
-	
-}
-div.main{
-	position: absolute;
-    right: 100px;
-	left: 140px;
-    width: 900px;
-	height: 900px;
-    border: 3px solid #8AC007;
-
-}
-div.items {
-	position: absolute;
-	right: 50px;
-    width: 400px;
-	height: 500px;
-    border: 3px solid #8AC007;
-	
-	
-}
-div.play {
-	position: absolute;
-	right: 470px;
-    width: 400px;
-	height: 500px;
-    border: 3px solid #8AC007;
-	background-color: grey;
-	
-	
-}
-
-table, td {
-    border-collapse: separate;
-    border-spacing: 15px;
-}
-
-td {
-    padding: 55px;
-	padding-right:20px;
-	background-color: red;
-
-	font-weight: bold;
-	text-align: center;
-	
-}
-td a{
-	text-decoration: none;
-	color: white;
-	text-align: center;
-	
-}
-td a:hover{
-	
-	color: #000000;
-}
-td a:visited{
-	color: #00ff00;
-}
-tr{
-	padding: 15px;
-}
-
-div.img {
-    
-    border: 1px solid #0000ff;
-    height: auto;
-    width: auto;
-    text-align: center;
-}	
-
-div.desc{
-	text-align: left;
-	font-weight: bold;
-	color: white;
-	width: 350px;
-	margin: 10px;
-}
-
-div.theatre{
-
-	position: absolute;
-	bottom: 240px;
-	right: 66px;
-	width: 810px;
-	height: 350px
-	border: 3px solid #8AC007;
-	background-color: grey;
-
-}
-
-div.deal{
-
-	position: absolute;
-	bottom: 70px;
-	right: 470px;
-    width: 400px;
-	height: 150px;
-    border: 3px solid #8AC007;
-	background-color: grey;
-
-}
-
-div.misc{
-	position: absolute;
-	bottom: 70px;
-	right: 50px;
-    width: 400px;
-	height: 150px;
-    border: 3px solid #8AC007;
-	
-	
-}
-
-#carousel {
-	width: 480px;
-	overflow:hidden;
-}
-
-#carousel ul {
-	width: 1920px;
-	padding: 0;
-	margin: 0;
-}
-
-#carousel ul li {
-	width:480px;
-	text-align: center;
-	height: 280px;
-	list-style: none;
-	float:	left;
-}
-</style>
 </head>
-<body>
+<body class=index_body>
 <div>
 <?php include 'header.php'; ?>
  </div>
   
- 
 
-<div class=main>
+<div class=index_main>
 
-<div class=play>
+<div class=index-play>
 
-	<div class=img>
+	<div id=index-gal>
 	
-	<img src="Images\MainPlay.png" width="380" height="360">
-	
-	<div class=desc>The new comedy An Act of God reveals the mysteries of the Bible while answering the existential questions that have plagued mankind since Creation in just 90 minutes. The One with the first and last word on everything is on Broadway to set the record straight�and He's not holding back!</div>
+		<img src="Images\MainPlay.png" class="active"/>
+		<img src="Images\emp.jpg"/>
+		<img src="Images\statLib.jpg"/>
 	
 	</div>
 	
-	
 </div>
  
- <div class=items>
+ <div class=index_items>
  
  <table>
-  <tr>
-    <td><a target="_blank" href="http://google.com/">Tours</a></td>
-    <td><a target="_blank" href="http://google.com/">Sports & Concerts</a></td>
-  </tr>
-  <tr>
-    <td><a target="_blank" href="http://google.com/">Museums Attractions</a></td>
-    <td><a target="_blank" href="http://google.com/">Airport Transportation</a></td>
-  </tr>
+	<tr>
+		<td class="tours"><a target="_blank" href="http://google.com/">Tours</a></td>
+		<td class="sports"><a target="_blank" href="http://google.com/" style="text-align: center;">Sports<br>&<br>Concerts</a></td>
+	</tr>
+	<tr>
+		<td class="museum"><a target="_blank" href="http://google.com/" style="text-align: center;">Museums<br>Attractions</a></td>
+		<td class="airport"><a target="_blank" href="http://google.com/" style="text-align: center;">Airport<br><center>Transportation</center></a></td>
+	</tr>
   
-  <tr>
-    <td><a target="_blank" href="http://google.com/">Dance Opera Ballet</td>
-    <td><a target="_blank" href="http://google.com/">Day Trips</a></td>
-  </tr>
-  
-  </table>
- </div>
- 
-  <div class=theatre>
- 
- <h1> Text </h1>
- <h1> Text</h1>
- 
- </div>
- 
- <div class=deal>
- 
- <h1> Text </h1>
- <h1> Text</h1>
- 
- </div>
- 
- <div class=misc>
- <table width=100%>
-  <tr>
-    <td><a target="_blank" href="http://google.com/">About Us</a></td>
-    <td><a target="_blank" href="http://google.com/">Contacts</a></td>
-  </tr>
-  </table>
- 
- </div>
- 
- 
- 
- </div>
- 
+	<tr>
+		<td class="dance"><a target="_blank" href="http://google.com/">Opera<br>Ballet<br>Symphony</td>
+		<td class="daytrip"><a target="_blank" href="http://google.com/">Day Trips</a></td>
+	</tr>
 
+  
+ </table>
+ 
+ </div>
+
+ <div id=index_theatre>
+	<button class="prev">&laquo;</button>
+		
+	<ul>
+		<li><img src="Images\1.png"><li>
+		<li><img src="Images\2.png"></li>
+		<li><img src="Images\3.png"></li>
+		<li><img src="Images\4.png"></li>
+		<li><img src="Images\5.png"></li>
+		<li><img src="Images\6.png"></li>
+		<li><img src="Images\7.png"></li>
+		<li><img src="Images\8.png"></li>
+		<li><img src="Images\9.png"></li>
+		<li><img src="Images\10.png"></li>
+		<li><img src="Images\11.png"></li>
+		<li><img src="Images\12.png"></li>
+		<li><img src="Images\13.png"></li>
+		<li><img src="Images\14.png"></li>
+		<li><img src="Images\15.png"></li>
+		<li><img src="Images\16.png"></li>
+		<li><img src="Images\17.png"></li>
+		<li><img src="Images\18.png"></li>
+		<li><img src="Images\19.png"></li>
+		<li><img src="Images\20.png"></li>
+		<li><img src="Images\21.png"></li>
+		<li><img src="Images\22.png"></li>
+		<li><img src="Images\23.png"></li>
+		<li><img src="Images\24.png"></li>
+		<li><img src="Images\25.png"></li>
+		<li><img src="Images\26.png"></li>
+		<li><img src="Images\27.png"></li>
+		<li><img src="Images\28.png"></li>
+		<li><img src="Images\29.png"></li>
+		<li><img src="Images\30.png"></li>
+	</ul>
+	
+	<button class="next">&rsaquo;</button>
+	
+ </div>
+
+ <div class=index_deal>
+ 
+	<a target="_blank" href="http://google.com/" width="100%">Deal of the day</a>
+ 
+ </div>
+ 
+ <div class=index_misc>
+	<div class=index_desc>The new comedy An Act of God reveals the mysteries of the Bible while answering the existential questions that have plagued mankind since Creation in just 90 minutes. The One with the first and last word on everything is on Broadway to set the record straight and He's not holding back</div>
+ 
+ </div>
+  
+ </div>
  
  <div>
- <?php include 'footer.php'; ?>
+	<?php include 'footer.php'; ?>
  </div>
 
 </body>
